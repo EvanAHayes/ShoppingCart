f48e75a1990cb3975317db60d6781b38e9b3d3c4
evanhayes <evanhayes66@yahoo.com>
2021-01-08 18:54:33 -0500

    enter

---
+++ src/containers/ProductLandingPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProductLandingPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ProductLandingPage;
