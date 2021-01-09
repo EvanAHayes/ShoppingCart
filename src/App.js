65cf7b88212e1ff9ffd8b5f5a077e96ec40a690d
evanhayes <evanhayes66@yahoo.com>
2021-01-09 09:25:22 -0500

    enter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React from 'react';
 import ProductLandingPage from './containers/ProductLandingPage';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Navbar from './components/NavBar/Navbar';
 
 
 function App() {
   return (
-    <div className="App">
-       <ProductLandingPage />
+    <div>
+       <BrowserRouter>
+       <Navbar>
+        <Switch>
+        <Route path="/" exact component={ProductLandingPage} />
+        </Switch>
+       </Navbar>
+       </BrowserRouter>
     </div>
   );
 }
--- src/containers/ProductLandingPage.jsx
+++ src/containers/ProductLandingPage.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import Navbar from '../components/NavBar/Navbar'
 import Products from '../components/Products/Products'
 
 class ProductLandingPage extends Component {
     render() {
         return (
             <div>
-                <Navbar />
                 <Products />
             </div>
         )
